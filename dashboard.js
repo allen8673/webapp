be3b394107403e40c3ff53e9c71de55b087bf954
allen8673 <allen8673@gmail.com>
2024-10-21 13:28:43 +0800

    add webapp

---
+++ dashboard.js
@@ -0,0 +1,17 @@
+function tabOnclick(evt, tabId) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    document.getElementById(tabId).style.display = "block";
+    if (!!evt?.currentTarget) {
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        evt.currentTarget.className += " active";
+    }
+}
+
+tabOnclick(undefined, 'dashboard_1')
